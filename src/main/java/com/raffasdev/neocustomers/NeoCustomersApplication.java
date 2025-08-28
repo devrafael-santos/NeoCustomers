31b1491383a9f867402c3c9fd0991f8e320bf6fe
devrafael-santos <raafael.cs@gmail.com>
2025-08-28 09:33:38 -0300

    setup project

---
+++ src/main/java/com/raffasdev/neocustomers/NeoCustomersApplication.java
@@ -0,0 +1,13 @@
+package com.raffasdev.neocustomers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeoCustomersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NeoCustomersApplication.class, args);
+    }
+
+}
